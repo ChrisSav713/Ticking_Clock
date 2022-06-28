d52c7eaa7c10e8abb6141878ce7fa81a13c4134c
ChrisSav713 <106366587+ChrisSav713@users.noreply.github.com>
2022-06-28 13:16:14 -0400

    done

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ function getSecondsSinceStartOfDay() {
 }
 
 setInterval(function() {
-    var time = getSecondsSinceStartOfDay();
-    console.log(time);
-    // your code here
+    // var time = getSecondsSinceStartOfDay();
+    // console.log(time);
+
+    let seconds = new Date().getSeconds();
+    let minutes = new Date().getMinutes();
+    let hours = new Date().getHours();
+    console.log(hours + " " + minutes + " " + seconds);
+    var degreesSeconds = ((seconds / 60) * 360);
+    var degreesMinutes = (minutes / 60) * 360;
+    var degreesHours = (hours / 12) * 360;
+    console.log(degreesHours + " " + degreesMinutes + " " + degreesSeconds);
+    document.getElementById("seconds").style.transform = "rotate(" + (degreesSeconds + 180) + "deg)";
+    document.getElementById("minutes").style.transform = "rotate(" + (degreesMinutes + 180) + "deg)";
+    document.getElementById("hour").style.transform = "rotate(" + (degreesHours + 180) + "deg)";
 }, 1000);
